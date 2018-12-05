d41bc91119c60720b5a9d5c2f346b5e6c0da57a6
Sarunas VASKELIS <svaskeli@e2r13p9.42.fr>
2018-12-05 18:34:53 +0100

    started the main with printf function and modified .h file

--- ft_printf.h
+++ ft_printf.h
@@ -6,12 +6,28 @@
 /*   By: dfinnis <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 15:06:08 by dfinnis           #+#    #+#             */
-/*   Updated: 2018/12/05 15:06:11 by dfinnis          ###   ########.fr       */
+/*   Updated: 2018/12/05 18:34:03 by svaskeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF
 # define FT_PRINTF
+
 # include "libft/libft.h"
+# include <string.h> //?
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h> //for testing
+
+typedef struct		s_list
+{
+	size_t			count;
+	size_t			len;
+	char			*flags;
+}					t_list;
+
+//t_print			ft_parse(char const *format, t_list all);
+t_list				ft_init(t_list all);
 
 #endif
---
+++ main.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svaskeli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/05 09:15:30 by svaskeli          #+#    #+#             */
+/*   Updated: 2018/12/05 18:27:13 by svaskeli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> //for testing
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "ft_printf.h"
+
+// TO DO
+// write the function base to deal with multiple arguments
+// the functions goes through the first argument and prints until % or '\0'
+// if there is %, then I call the funtion
+
+// MANDATORY
+// can use: write, malloc, free, exit, <stdarg.h>
+// conversions: c, s, p
+// conversions: d, i, o, u, x, X with flags: hh, h, l, ll
+// conversion f with flags L and l
+// flags: #, 0, -, +, space, min width, precision
+// %%
+
+// BONUS
+// conversions: eg with L flags
+// flags: *$'
+// %b for binary, %r for non printables, %k for date
+// colors, fd, etc.
+
+t_list	ft_init(t_list all)
+{
+	all.count = 0;
+	all.len = 0;
+	all.flags = NULL;
+	return (all);
+}
+
+int 	ft_printf(char const *format, ...)
+{
+	va_list ap;
+	t_list	all;
+	int		i;
+	int		count;
+
+	count = 0;
+	va_start(ap, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			all = ft_init(all);
+//			all = ft_parse(format, all);
+//			all = ft_convert(all);
+			format = format + (all.len - 1);
+			count = count + all.count;
+		}
+		else
+		{
+			write(1, (char*)format, 1);
+			count++;
+		}
+		format++;
+	}
+	va_end(ap);
+	return (count);
+}
+
+int 	main(void)
+{
+	printf("real\n");
+	ft_printf("abc\n");
+}
