16d360af3f8fd71716627ac18323b28bbdd79d7b
Sarunas VASKELIS <svaskeli@e2r13p14.42.fr>
2018-12-06 17:03:28 +0100

    edits to convert.c - working with numbers

--- convert.c
+++ convert.c
@@ -6,7 +6,7 @@
 /*   By: svaskeli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 18:51:39 by svaskeli          #+#    #+#             */
-/*   Updated: 2018/12/06 15:55:58 by svaskeli         ###   ########.fr       */
+/*   Updated: 2018/12/06 17:02:33 by svaskeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,42 +25,74 @@ void		ft_fill_width(int len, t_print all, char c)
 		}
 }
 
-void		ft_space(char *str, t_print all)
+void		ft_justif(char *num_str, t_print all)
 {
-	if (str[0] != '-')
-		write(1, " ", 1);
+	if (all.minus == 1)
+		ft_putstr(num_str);
+	if (all.width != 0 && !all.zero)
+		ft_fill_width(ft_strlen(num_str), all, ' ');
+	if ((all.width != 0 && all.zero == 1) || all.precision)
+		ft_fill_width(ft_strlen(num_str), all, '0');
+	if (all.minus == 0)
+		ft_putstr(num_str);
+	all.count = all.count + ft_strlen(num_str) + all.width;
+}
+
+void		ft_intplus(char *num_str, t_print all)
+{
+	if (num_str[0] =! '-')
+	{
+		if (all.plus)
+			write(1, "+", 1);
+		if (all.space)
+			write(1, " ", 1);
+		all.count++;
+	}
+	ft_putstr(num_str);
+	all.count = all.count + ft_strlen(num_str);
 }
 
 //modifying d, i and u without h, hh, l and ll
 t_print		ft_modify_int(t_print all, va_list ap)
 {
 	char	*num_str;
-	int		i;
 	int		num;
 
 	num = va_arg(ap, int);
-	i = 0;
 	if (!(num_str = ft_itoa(num)))
-		return (all); //error
-	if (all.minus == 1)
-		ft_putstr(num_str);
-	if (all.width != 0 && !all.zero)
-		ft_fill_width(ft_strlen(num_str), all, ' ');
-	if ((all.width != 0 && all.zero == 1) || all.precision != 0)
-		ft_fill_width(ft_strlen(num_str), all, '0');
-	if (all.minus == 0)
-		ft_putstr(num_str);
-	all.count = ft_strlen(num_str) + all.width;
+		return (all); //ft_error
+	if (!all.plus || !all.space) //flags: + and space
+		ft_intplus(num_str, all);
+	if (!all.minus || !all.zero || !all.width) //flags: 0, -, min_width
+		ft_justif(num_str, all);
 	free(num_str);
 	return (all);
 }
 
+void		ft_int(t_print all, va_list ap)
+{
+	if (all.l)
+		ft_long_int(all, ap);//to write
+	else if (all.ll)
+		ft_long_long_int(all, ap);//to write
+	else if (all.h)
+		ft_short_int(all, ap);//to write
+	else if (all.hh)
+		ft_unsigned_int(all, ap);//to write
+	else
+		ft_modify_int(all, ap);
+}
+
 t_print		ft_number(t_print all, va_list ap)
 {
-	if ((all.type == 'i' || all.type == 'd' || all.type == 'u') && !all.h &&
-			!all.hh && !all.l && !all.ll)
-		ft_modify_int(all, ap);
-	if ...
+	if (all.type == 'i' || all.type == 'd' || all.type == 'u')
+		ft_int(all, ap);
+	if (all.type == 'o')
+		ft_int_octal(all, ap);
+	if (all.type == 'x' || all.type == 'X')
+		ft_int_hex(all, ap);
+	if (all.type =='f')
+		ft_int_float(all, ap);
 	return (all);
 }
 
