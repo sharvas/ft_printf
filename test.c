458e3b10beba324991a1a01304350311342dd43b
Sarunas VASKELIS <svaskeli@student.42.fr>
2018-12-08 18:32:42 +0100

    test

--- test.c
+++ test.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include "libft/libft.h"
 
 int main(void)
 {
 	char *p;
+	char *num;
 
+//	num = ft_itoa_base(p, 16, 'x');
 	p = strdup("hhh");
-	printf("%8p\n", p);
-	p = strcat(p, "x");
-	printf("%8p\n", p);
+//	write(1, &num, 16);
+	printf("%p\n", p);
+	printf("0x%s\n", ft_itoa_base((unsigned long)p, 16, 'x'));
 }
