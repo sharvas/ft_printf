58e2f85759376ab5565066c8d326d5c96cc35d56
Sarunas VASKELIS <svaskeli@student.42.fr>
2018-12-09 13:56:12 +0100

    itoa

---
+++ ft_itoa_base.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dfinnis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/06 10:02:28 by dfinnis           #+#    #+#             */
+/*   Updated: 2018/12/08 17:44:08 by svaskeli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_find_len(unsigned long long n, int b)
+{
+	int			len;
+
+	len = 1;
+	while ((n /= b) > 0)
+		len++;
+	return (len);
+}
+
+char		*ft_itoa_base(unsigned long long n, int b, char type)
+{
+	char	*base;
+	char	*fresh;
+	int		len;
+
+	if (type == 'X')
+	{
+		if (!(base = ft_strdup("0123456789ABCDEF")))
+			return (NULL);
+	}
+	else
+	{
+		if (!(base = ft_strdup("0123456789abcdef")))
+			return (NULL);
+	}
+	len = ft_find_len(n, b);
+	if (!(fresh = ft_strnew(len)))
+		return (NULL);
+	if (n == 0)
+		fresh[0] = '0';
+	fresh[len + 1] = '\0';
+	while (n > 0)
+	{
+		fresh[--len] = base[n % b];
+		n = n / b;
+	}
+	return (fresh);
+}
